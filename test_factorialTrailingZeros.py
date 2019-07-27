c09b33e1d8f844a1cf511306764940ff4b35ea36
hemal507 <hchowdam@gmail.com>
2019-07-27 19:03:08 +0530

    Add files via upload

---
+++ test_factorialTrailingZeros.py
@@ -0,0 +1,11 @@
+import factorialTrailingZeros
+
+def test_case1() :
+	result = factorialTrailingZeros.factorialTrailingZeros(10)
+	assert result == 2 
+
+def test_case2() :
+	result = factorialTrailingZeros.factorialTrailingZeros(4)
+	assert result == 0
+
+
