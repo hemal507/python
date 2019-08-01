62beb4da099779255e75713f5bae119bf2152ed6
hemal507 <hchowdam@gmail.com>
2019-08-01 07:24:52 +0530

    Add files via upload

---
+++ test_adjacentElementsProduct.py
@@ -0,0 +1,8 @@
+import adjacentElementsProduct
+
+def test_case1() :
+	assert adjacentElementsProduct.adjacentElementsProduct([3,4,6,1,6,7,8,2,4]) == 56
+
+def test_case2() :
+	assert adjacentElementsProduct.adjacentElementsProduct([9,5,6,7,9,9,11,12,4,5,0]) == 132
+
