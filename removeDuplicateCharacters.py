fce42addd1f4581c4484449733a65c75cafd1846
hemal507 <hchowdam@gmail.com>
2019-08-02 22:09:13 +0530

    Update removeDuplicateCharacters.py

--- removeDuplicateCharacters.py
+++ removeDuplicateCharacters.py
@@ -1,12 +1,3 @@
 def removeDuplicateCharacters(string) :
 	return ''.join([x for x in string if string.count(x) == 1  ])
 
-
-'''
-def removeDuplicateCharacters(str: String): String = {
-    str.filter(i => str.count(_ == i) == 1)
-      .mkString("")
-}
-
-'''
-
