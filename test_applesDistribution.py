002d53d30d14058a6736d6b136b0926df3bc85b3
hemal507 <hchowdam@gmail.com>
2019-08-08 00:05:34 +0530

    Add files via upload

---
+++ test_applesDistribution.py
@@ -0,0 +1,10 @@
+import applesDistribution
+
+def test_case1() :
+	assert applesDistribution.applesDistribution(7,4,1) == 3
+
+def test_case2() :
+	assert applesDistribution.applesDistribution(10,5,1) == 4
+
+def test_case3() :
+	assert applesDistribution.applesDistribution(4,4,3) == 4 
