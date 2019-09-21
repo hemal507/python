50d0deb726aa3a244debb6d8611d41ad1cb72e98
hemal507 <hchowdam@gmail.com>
2019-09-21 10:15:53 +0530

    Add files via upload

---
+++ memoryPills.py
@@ -0,0 +1,10 @@
+from itertools import dropwhile
+
+def memoryPills(pills):
+    gen = dropwhile(lambda x : len(x) % 2 != 0 , pills + [""] * 3 )
+    next(gen)
+    return [next(gen) for _ in range(3)]
+
+pills= ["Notforgetan",  "Antimoron",  "Rememberin",  "Bestmedicen",  "Superpillsus"]
+
+print(memoryPills(pills))
