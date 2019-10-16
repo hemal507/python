ffb00d5c5c0edc6b686ee649d1493841536c9b76
hemal507 <hchowdam@gmail.com>
2019-10-16 22:04:45 +0530

    Add files via upload

---
+++ test_digitSum.py
@@ -0,0 +1,10 @@
+import digitSum
+
+def test_case1() :
+	assert digitSum.digitSum(111) == 3
+
+def test_case2() :
+        assert digitSum.digitSum(0) == 0
+
+def test_case3() :
+        assert digitSum.digitSum(349859435) == 50
